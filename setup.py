bf2135462edc1b4804c21f9d6e3a0e9ec9eb9f80
Hai Luan Nguyen <hailuan@Celandine.lan>
2017-06-22 13:17:27 +0200

    Version 0.0.3

---
+++ setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup, find_packages
+from codecs import open
+from os import path
+
+__version__ = '0.0.2'
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+
+install_requires = ['sphinx', 'flake8', 'coverage', 'pygit2']
+dependency_links = []
+
+setup(
+    name='gitkv',
+    version=__version__,
+    description='Use a git repo as a key-value store.',
+    long_description=long_description,
+    url='https://github.com/edouardklein/gitkv',
+    download_url='https://github.com/edouardklein/gitkv/tarball/' + __version__,
+    license='AGPLv3',
+    classifiers=[
+	'Programming Language :: Python :: 3.4',
+    ],
+    keywords='gitkv use a git repo as a key-value store.',
+    packages=['gitkv'],
+    include_package_data=True,
+    author='HaiLuan Nguyen, Edouard Klein',
+    install_requires=install_requires,
+    dependency_links=dependency_links,
+    author_email='edouardklein@gmailnospam.com'
+)
